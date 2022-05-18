386b9e94c02c9a33b50e5dfd570a5559f69ff990
Evans Jimenez <evansjimenez22@gmail.com>
2022-05-18 12:04:25 -0400

    Lo principal del CRUD

--- CrudForm/Form1.Designer.cs
+++ CrudForm/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace CrudForm
 {
-    partial class Form1
+    partial class CrudForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,314 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblID = new System.Windows.Forms.Label();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.lblApellido = new System.Windows.Forms.Label();
+            this.txtDireccion = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtCiudad = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPais = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtCelular = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnAñadir = new System.Windows.Forms.Button();
+            this.btnActualizar = new System.Windows.Forms.Button();
+            this.btnBorrar = new System.Windows.Forms.Button();
+            this.btnCargar = new System.Windows.Forms.Button();
+            this.DGVContacts = new System.Windows.Forms.DataGridView();
+            this.txtCorreo = new System.Windows.Forms.TextBox();
+            this.Correo = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.DGVContacts)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblID
+            // 
+            this.lblID.AutoSize = true;
+            this.lblID.Enabled = false;
+            this.lblID.Font = new System.Drawing.Font("Cambria", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblID.Location = new System.Drawing.Point(75, 19);
+            this.lblID.Name = "lblID";
+            this.lblID.Size = new System.Drawing.Size(26, 20);
+            this.lblID.TabIndex = 0;
+            this.lblID.Text = "ID";
+            // 
+            // txtID
+            // 
+            this.txtID.BackColor = System.Drawing.SystemColors.Window;
+            this.txtID.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtID.Enabled = false;
+            this.txtID.Location = new System.Drawing.Point(117, 19);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(221, 22);
+            this.txtID.TabIndex = 1;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.BackColor = System.Drawing.SystemColors.Window;
+            this.txtNombre.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtNombre.Enabled = false;
+            this.txtNombre.Location = new System.Drawing.Point(117, 71);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(221, 22);
+            this.txtNombre.TabIndex = 3;
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Enabled = false;
+            this.lblNombre.Font = new System.Drawing.Font("Cambria", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombre.Location = new System.Drawing.Point(33, 71);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(68, 20);
+            this.lblNombre.TabIndex = 2;
+            this.lblNombre.Text = "Nombre";
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.BackColor = System.Drawing.SystemColors.Window;
+            this.txtApellido.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtApellido.Enabled = false;
+            this.txtApellido.Location = new System.Drawing.Point(117, 121);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(221, 22);
+            this.txtApellido.TabIndex = 5;
+            // 
+            // lblApellido
+            // 
+            this.lblApellido.AutoSize = true;
+            this.lblApellido.Enabled = false;
+            this.lblApellido.Font = new System.Drawing.Font("Cambria", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblApellido.Location = new System.Drawing.Point(32, 124);
+            this.lblApellido.Name = "lblApellido";
+            this.lblApellido.Size = new System.Drawing.Size(70, 20);
+            this.lblApellido.TabIndex = 4;
+            this.lblApellido.Text = "Apellido";
+            // 
+            // txtDireccion
+            // 
+            this.txtDireccion.BackColor = System.Drawing.SystemColors.Window;
+            this.txtDireccion.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtDireccion.Enabled = false;
+            this.txtDireccion.Location = new System.Drawing.Point(117, 158);
+            this.txtDireccion.MaximumSize = new System.Drawing.Size(500, 500);
+            this.txtDireccion.Multiline = true;
+            this.txtDireccion.Name = "txtDireccion";
+            this.txtDireccion.Size = new System.Drawing.Size(500, 200);
+            this.txtDireccion.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Enabled = false;
+            this.label1.Font = new System.Drawing.Font("Cambria", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(26, 158);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Direccion";
+            // 
+            // txtCiudad
+            // 
+            this.txtCiudad.BackColor = System.Drawing.SystemColors.Window;
+            this.txtCiudad.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtCiudad.Enabled = false;
+            this.txtCiudad.Location = new System.Drawing.Point(116, 369);
+            this.txtCiudad.Name = "txtCiudad";
+            this.txtCiudad.Size = new System.Drawing.Size(222, 22);
+            this.txtCiudad.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Enabled = false;
+            this.label2.Font = new System.Drawing.Font("Cambria", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(42, 371);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 20);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Cuidad";
+            // 
+            // txtPais
+            // 
+            this.txtPais.BackColor = System.Drawing.SystemColors.Window;
+            this.txtPais.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtPais.Enabled = false;
+            this.txtPais.Location = new System.Drawing.Point(116, 415);
+            this.txtPais.Name = "txtPais";
+            this.txtPais.Size = new System.Drawing.Size(222, 22);
+            this.txtPais.TabIndex = 11;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Enabled = false;
+            this.label3.Font = new System.Drawing.Font("Cambria", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(53, 417);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 20);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Pais";
+            // 
+            // txtCelular
+            // 
+            this.txtCelular.BackColor = System.Drawing.SystemColors.Window;
+            this.txtCelular.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtCelular.Enabled = false;
+            this.txtCelular.Location = new System.Drawing.Point(117, 457);
+            this.txtCelular.Name = "txtCelular";
+            this.txtCelular.Size = new System.Drawing.Size(114, 22);
+            this.txtCelular.TabIndex = 13;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Enabled = false;
+            this.label4.Font = new System.Drawing.Font("Cambria", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(43, 459);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 20);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Celular";
+            // 
+            // btnAñadir
+            // 
+            this.btnAñadir.BackColor = System.Drawing.Color.ForestGreen;
+            this.btnAñadir.Font = new System.Drawing.Font("Cambria", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAñadir.Location = new System.Drawing.Point(456, 371);
+            this.btnAñadir.Name = "btnAñadir";
+            this.btnAñadir.Size = new System.Drawing.Size(111, 40);
+            this.btnAñadir.TabIndex = 14;
+            this.btnAñadir.Text = "Añadir";
+            this.btnAñadir.UseVisualStyleBackColor = false;
+            this.btnAñadir.Click += new System.EventHandler(this.btnAñadir_Click);
+            // 
+            // btnActualizar
+            // 
+            this.btnActualizar.BackColor = System.Drawing.Color.Wheat;
+            this.btnActualizar.Font = new System.Drawing.Font("Cambria", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnActualizar.Location = new System.Drawing.Point(456, 449);
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(111, 40);
+            this.btnActualizar.TabIndex = 15;
+            this.btnActualizar.Text = "Actualizar";
+            this.btnActualizar.UseVisualStyleBackColor = false;
+            this.btnActualizar.Click += new System.EventHandler(this.btnActualizar_Click);
+            // 
+            // btnBorrar
+            // 
+            this.btnBorrar.BackColor = System.Drawing.Color.Brown;
+            this.btnBorrar.Font = new System.Drawing.Font("Cambria", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBorrar.Location = new System.Drawing.Point(584, 371);
+            this.btnBorrar.Name = "btnBorrar";
+            this.btnBorrar.Size = new System.Drawing.Size(103, 40);
+            this.btnBorrar.TabIndex = 16;
+            this.btnBorrar.Text = "Borrar";
+            this.btnBorrar.UseVisualStyleBackColor = false;
+            // 
+            // btnCargar
+            // 
+            this.btnCargar.BackColor = System.Drawing.Color.AliceBlue;
+            this.btnCargar.Font = new System.Drawing.Font("Cambria", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCargar.Location = new System.Drawing.Point(584, 449);
+            this.btnCargar.Name = "btnCargar";
+            this.btnCargar.Size = new System.Drawing.Size(103, 40);
+            this.btnCargar.TabIndex = 17;
+            this.btnCargar.Text = "Cargar\r\n";
+            this.btnCargar.UseVisualStyleBackColor = false;
+            this.btnCargar.Click += new System.EventHandler(this.btnCargar_Click);
+            // 
+            // DGVContacts
+            // 
+            this.DGVContacts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DGVContacts.Location = new System.Drawing.Point(708, 54);
+            this.DGVContacts.Name = "DGVContacts";
+            this.DGVContacts.RowHeadersWidth = 51;
+            this.DGVContacts.RowTemplate.Height = 24;
+            this.DGVContacts.Size = new System.Drawing.Size(563, 436);
+            this.DGVContacts.TabIndex = 18;
+            // 
+            // txtCorreo
+            // 
+            this.txtCorreo.BackColor = System.Drawing.SystemColors.Window;
+            this.txtCorreo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtCorreo.Enabled = false;
+            this.txtCorreo.Location = new System.Drawing.Point(310, 457);
+            this.txtCorreo.Name = "txtCorreo";
+            this.txtCorreo.Size = new System.Drawing.Size(140, 22);
+            this.txtCorreo.TabIndex = 20;
+            // 
+            // Correo
+            // 
+            this.Correo.AutoSize = true;
+            this.Correo.Enabled = false;
+            this.Correo.Font = new System.Drawing.Font("Cambria", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Correo.Location = new System.Drawing.Point(245, 459);
+            this.Correo.Name = "Correo";
+            this.Correo.Size = new System.Drawing.Size(59, 20);
+            this.Correo.TabIndex = 19;
+            this.Correo.Text = "Correo";
+            // 
+            // CrudForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1316, 502);
+            this.Controls.Add(this.txtCorreo);
+            this.Controls.Add(this.Correo);
+            this.Controls.Add(this.DGVContacts);
+            this.Controls.Add(this.btnCargar);
+            this.Controls.Add(this.btnBorrar);
+            this.Controls.Add(this.btnActualizar);
+            this.Controls.Add(this.btnAñadir);
+            this.Controls.Add(this.txtCelular);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtPais);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtCiudad);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtDireccion);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtApellido);
+            this.Controls.Add(this.lblApellido);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.lblID);
+            this.Name = "CrudForm";
+            this.Text = "CrudForm";
+            ((System.ComponentModel.ISupportInitialize)(this.DGVContacts)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblID;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.Label lblApellido;
+        private System.Windows.Forms.TextBox txtDireccion;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtCiudad;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPais;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtCelular;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnAñadir;
+        private System.Windows.Forms.Button btnActualizar;
+        private System.Windows.Forms.Button btnBorrar;
+        private System.Windows.Forms.Button btnCargar;
+        private System.Windows.Forms.DataGridView DGVContacts;
+        private System.Windows.Forms.TextBox txtCorreo;
+        private System.Windows.Forms.Label Correo;
     }
 }
 
--- CrudForm/Form1.cs
+++ CrudForm/Form1.cs
@@ -10,11 +10,122 @@ using System.Windows.Forms;
 
 namespace CrudForm
 {
-    public partial class Form1 : Form
+    public partial class CrudForm : System.Windows.Forms.Form
     {
-        public Form1()
+        public Boolean Adding = false;
+        List<Contact> Contacts = new List<Contact>();
+        public CrudForm()
         {
             InitializeComponent();
         }
+
+        private void btnAñadir_Click(object sender, EventArgs e)
+        {
+            EmptyControls();
+            EnableControls(true);
+            Adding = true;
+            txtID.Text = Guid.NewGuid().ToString();
+            btnAñadir.Enabled = false;
+            btnBorrar.Enabled = false;
+            btnCargar.Enabled = false;
+        }
+
+        private void EmptyControls()
+        {
+            txtID.Text = string.Empty;
+            txtNombre.Text = string.Empty;
+            txtApellido.Text = string.Empty;
+            txtID.Text = string.Empty;
+            txtDireccion.Text = string.Empty;
+            txtCiudad.Text = string.Empty;
+            txtCorreo.Text = string.Empty;
+            txtCelular.Text = string.Empty;
+            txtPais.Text = string.Empty;
+        }
+
+        private void EnableControls(bool enabled)
+        {
+            txtNombre.Enabled = enabled;
+            txtApellido.Enabled = enabled;
+            txtID.Enabled = enabled;
+            txtDireccion.Enabled = enabled;
+            txtCiudad.Enabled = enabled;
+            txtCorreo.Enabled = enabled;
+            txtCelular.Enabled = enabled;
+            txtPais.Enabled = enabled;
+        }
+
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+            Save();
+        }
+
+        private void Save()
+        {
+            if (Adding == true)
+            {
+                //Se añade un nuevo registro
+                var contact = new Contact
+                {
+                    ID = txtID.Text,
+                    Nombre = txtNombre.Text,
+                    Apellido = txtApellido.Text,
+                    Dirrecion = txtDireccion.Text,
+                    Ciudad = txtCiudad.Text,
+                    Pais = txtPais.Text,
+                    Celular = txtCelular.Text,
+                    Correo = txtCorreo.Text,
+                };
+
+                Contacts.Add(contact);
+
+                MessageBox.Show("Contacto Registrado.");
+
+                EmptyControls();
+                EnableControls(false);
+
+                btnAñadir.Enabled = true;
+                btnBorrar.Enabled = true;
+                btnActualizar.Enabled = true;
+
+                GetContacts();
+
+            }
+            else
+            {
+                //se actualiza un registro ya existente
+            }
+        }
+
+        private void btnCargar_Click(object sender, EventArgs e)
+        {
+            GetContacts();
+        }
+
+        private void GetContacts()
+        {
+            DGVContacts.DataSource = null;
+            DGVContacts.DataSource = Contacts;
+        }
+    }
+
+    public class Contact
+    {
+        public string ID { get; set; }
+        
+        public string Nombre { get; set;}
+        
+        public string Apellido { get; set;}
+        
+        public string Dirrecion { get; set;}
+        
+        public string Pais { get; set;}
+        
+        public string Ciudad { get; set;} 
+        
+        public string Celular { get; set;}
+        
+        public string Correo { get; set;}
+        
     }
 }
